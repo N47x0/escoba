cd661087e36606f3ea68a81931d13aef8318c8f3
Nacho <nacho@sentiar.com>
2018-11-11 23:12:38 -0600

    start impl game loop

--- escoba.py
+++ escoba.py
@@ -21,19 +21,26 @@ def shuffle(deck :list):
   return deck
 
 def deal(deck :list, n=1):
-  return
+  return NotImplemented
+
+def deal_start(deck :list):
+  p1,p2, deck = deal_hand(deck)
+  start_table_cards = deck[:4]
+  deck = deck[4:]
+  return p1,p2,start_table_cards, deck
 
 def deal_hand(deck :list):
   count = 0
   p1 = []
   p2 = []
+  # deal out to p1 and p2 alternating
   for card in deck[:6]:
     if count % 2 == 0:
       p1 += card
     else:
       p2 += card
     count += 1
-  return p1,p2
+  return p1,p2,deck
 
 def get_combinations(cards: list):
   combos = []
@@ -50,8 +57,15 @@ def valid_plays(cards , player_id):
     if ( s == 15):
       plays.append(set(combo))
   
-  print("plays {}".format(plays))
-      
+  return plays
+
+def player_rewards(card_store):
+  # point if more O cards
+  # point if more cards
+  # point if 3+ 7s
+  # point if 7O card
+  # point when escoba but not here
+  return NotImplemented
 
 class Deck:
   card_store = {}
@@ -68,20 +82,34 @@ class Deck:
     d = self.deck_order[:n]
     self.deck_order = self.deck_order[n:]
     return Deck(make_deck_from(d))
+  def cards(self):
+    return self.card_store
+  def order(self):
+    return self.deck_order
+
+
 
 class Player: 
-  cards_won = Deck()
+  score = 0
   hand = Deck()
-  def __init__(self, deck=[]):
-    hand = deck
+  name = ''
+  def __init__(self, name, deck=[]):
+    self.name = name
   def play_turn(self,deck):
     return NotImplemented
   def play_card(self, deck):
-
     return NotImplemented
+  def award_point(self, points):
+    self.score += points
 
 class Game:
-  def __init__(self):
+  deck = Deck()
+  pl1 = Player('p1')
+  pl2 = Player('p2')
+  def __init__(self, pl1, pl2, deck):
+    self.pl1 = pl1
+    self.pl2 = pl2
+    self.deck = deck
     print("Start game")
     #return NotImplemented
 
@@ -89,6 +117,30 @@ class Game:
     return Deck()
   def play_card(self):
     return NotImplemented
+  def apply_play(self,play):
+    # validate(play)
+    # assign card owners
+    # award escoba
+    return NotImplemented
+  def play_game(self):
+    p1,p2,table_cards,self.deck = deal_start(self.deck)
+    for card in table_cards:
+      self.deck.cards()[card] = 'table'
+    for card in p1:
+      self.deck.cards()[card] = self.pl1.name
+    for card in p2:
+      self.deck.cards()[card] = self.pl2.name
+    
+    # main loop per player
+    while (len(p1) and len(p2)):
+      playable = valid_plays(self.deck.cards(), self.pl1.name)
+      self.apply_play(self.pl1.get_play(playable))
+      playable = valid_plays(self.deck.cards(), self.pl2.name)
+      self.apply_play(self.pl2.get_play(playable))
+
+      
+
+    
 
 if __name__ == "__main__":
   d = Deck(make_deck())
