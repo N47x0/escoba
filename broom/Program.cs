ab9f2c1c3b2a9e7d2898b8794ca6ee3f52eedd1a
shortpoet <soriano.carlitos@gmail.com>
2019-10-12 20:10:59 -0400

    added dotnet new webapi

--- broom/Program.cs
+++ broom/Program.cs
@@ -1,33 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using GameManager;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace broom
 {
-
-  class Program
-  {
-    static void Main(string[] args)
+    public class Program
     {
-      AutoGame();
-    }
-    static void AutoGame () {
-      
-      Game g = new Game();
-      Console.WriteLine("Starting a game of ESCOBA!");
-
-      int rounds = 0;
-      while (g.pl1.score < 15 && g.pl2.score < 15) {
-        rounds++;
-        if (rounds % 2 == 1) {
-          g.PlayRound(g.pl1, g.pl2);
-        } else {
-          g.PlayRound(g.pl2, g.pl1);
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
         }
-        Console.WriteLine($"Round {rounds}\t PL1: {g.pl1.score}\tPL2: {g.pl2.score}");
-      }
 
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
-  }
 }
